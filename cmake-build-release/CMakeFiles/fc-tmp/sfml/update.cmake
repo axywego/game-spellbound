f66d86c783ca921fc952687efd59f1a65dca9716
axywego <pukeotim@gmail.com>
2025-08-08 23:19:35 +0300

    git ignore real (^_^)

--- cmake-build-release/CMakeFiles/fc-tmp/sfml/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("D:/MY_PROJECTS/SPELLBOUND-GAME/cmake-build-release/CMakeFiles/fc-tmp/sfml/sfml-gitupdate.cmake")
-
-endblock()
